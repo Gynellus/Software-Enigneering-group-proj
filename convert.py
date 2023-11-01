55540342023c4043d7a91f637be9a9d43488b39d
Aleks Siedlecka <alekssiedlecka@gmail.com>
2023-11-01 13:50:10 +0100

    Correcting the case of url input

--- convert.py
+++ convert.py
@@ -2,6 +2,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import fitz
+from io import BytesIO
 
 def convert_input_to_string(input_data):
     # Input is .pdf
@@ -19,12 +20,21 @@ def convert_input_to_string(input_data):
 
     # Input is URL
     elif isinstance(input_data, str) and input_data.startswith(('http://', 'https://')):
-        response = requests.get(input_data)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            return soup.get_text()
-        else:
-            return 'Failed to fetch content from URL'
+        try:
+            response = requests.get(input_data)
+            response.raise_for_status()
+            content_type = response.headers['Content-Type']
+            if 'application/pdf' in content_type:
+                document = fitz.open(stream=BytesIO(response.content), filetype="pdf")
+                text = ''
+                for page in document:
+                    text+= page.get_text()
+                return text
+            else:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                return soup.get_text()
+        except Exception as e:
+            return f'Failed to fetch content from URL: {e}'
 
     # Input is a string (not a file path or URL)
     elif isinstance(input_data, str):
@@ -32,4 +42,6 @@ def convert_input_to_string(input_data):
 
     # Case 4: Other data types (e.g., numbers, lists, etc.)
     else:
-        return "Not one of the supported data formats (URL, .PDF. string)"
+        return "Unsupported data format"
+
+
