fe7008e7c6865fe7c5b12f7af0165f19420aba6e
Gynellus <guidomorera1998@gmail.com>
2023-11-08 11:28:26 +0100

    improved similarity finder, still prints instead of return (depends on integration needs)

--- find_similar_papers.py
+++ find_similar_papers.py
@@ -3,21 +3,20 @@ import json
 import numpy as np
 import PyPDF2
 import tensorflow_hub as hub
-import textwrap
-from tqdm import tqdm
 
-# Provided functions
+# Function to open a file and read its content
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
 
+# Function to calculate cosine similarity between two vectors
 def cosine_similarity(vec1, vec2):
     dot_product = np.dot(vec1, vec2)
     norm_vec1 = np.linalg.norm(vec1)
     norm_vec2 = np.linalg.norm(vec2)
     return dot_product / (norm_vec1 * norm_vec2)
 
-# Function to extract text from PDF
+# Function to extract text from a PDF file
 def extract_text_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as pdffileobj:
         pdfreader = PyPDF2.PdfReader(pdffileobj)
@@ -27,17 +26,13 @@ def extract_text_from_pdf(pdf_path):
             paper += pageobj.extract_text()
     return paper
 
-# Load precomputed embeddings and their corresponding article metadata
+# Generator function to load embeddings one by one to save memory
 def load_embeddings(directory):
-    embeddings = []
-    articles = []
     for filename in os.listdir(directory):
         if filename.endswith('.json'):
             with open(os.path.join(directory, filename), 'r') as f:
                 article = json.load(f)
-                articles.append(article)
-                embeddings.append(article['embedding'])
-    return articles, np.array(embeddings)
+                yield article['embedding'], article
 
 if __name__ == '__main__':
     # Load Universal Sentence Encoder
@@ -49,38 +44,23 @@ if __name__ == '__main__':
         pdf_path = os.path.join('pdfs/', pdf_file)
         paper_text = extract_text_from_pdf(pdf_path)
         
-        # Diagnostic print for paper_text
-        print("Extracted Paper Text (first 500 chars):")
-        print(textwrap.shorten(paper_text, 500))
-        
-        # Create an embedding for the extracted text
+        # Embedding for the extracted text
         paper_embedding = embed([paper_text]).numpy()
 
-        # Load the precomputed embeddings
-        articles, embeddings = load_embeddings('Data/embeddings')
+        # Initialize list to store similarities and corresponding articles
+        similarities = []
+        articles = []
 
-        # Compute the cosine similarity 
-        similarities = []
-        for e in tqdm(embeddings):
-            sim = cosine_similarity(paper_embedding[0], e) 
+        # Load and compare each embedding
+        for embedding, article in load_embeddings('Data/similarity_finder_Data/'):
+            sim = cosine_similarity(paper_embedding[0], embedding)
             similarities.append(sim)
-        
-        # Diagnostic prints for similarities
-        print(f"Similarities Min: {np.min(similarities)}, Max: {np.max(similarities)}, Mean: {np.mean(similarities)}")
-        
-        # Compute the cosine similarity 
-        similarities = []
-        for e in tqdm(embeddings):
-            sim = cosine_similarity(paper_embedding[0], e)
-            similarities.append(sim)
+            articles.append(article)
 
         # Get the top 5 most similar articles
         top_indices = np.argsort(similarities)[-5:][::-1]
-        top_articles = []
-        top_similarities = []
-        for idx in top_indices.tolist():
-            top_articles.append(articles[idx])
-            top_similarities.append(similarities[idx])
+        top_articles = [articles[idx] for idx in top_indices]
+        top_similarities = [similarities[idx] for idx in top_indices]
 
         # Display results
         print(f"Top 5 similar articles for {pdf_file}:")
